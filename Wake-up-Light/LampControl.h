4a9221efb381d90458b455bc4d829e2906e057db
BKayCode <19369093+BKayCode@users.noreply.github.com>
2024-11-11 19:39:26 +0100

    Update LampControl.h

--- Wake-up-Light/LampControl.h
+++ Wake-up-Light/LampControl.h
@@ -1,9 +1,9 @@
-#ifndef LampControl_h
-#define LampControl_h
+#ifndef lampControl_h
+#define lampControl_h
 #include <Arduino.h>
 #include <Wire.h>
 
-void startRise(byte Risetime, byte RiseBrightness);
+void startRise(byte risetime, byte riseBrightness);
 void sendBrightness(byte sendBrightness);
 
 #endif
